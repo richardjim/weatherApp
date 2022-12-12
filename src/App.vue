3a3b10fcde40c11c22473b52a3ff50845a5ec3fb
richardjim <richardiyevhobu@gmail.com>
2022-12-12 13:06:46 +0100

    set up nav ad tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <RouterView />
+    <SiteNavigation />
+
   </div>
 </template>
 
 <script>
-import RouterView from 'vue-router'
+import { RouterView } from 'vue-router'
+import SiteNavigation from './components/SiteNavigation.vue';
 export default {
-
+  components: { SiteNavigation }
 }
 </script>
